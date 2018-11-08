475cdc3f0b3c29d7adfce855387614a0fb600db1
Rodolfo Santana <rodisantana2002@gmail.com>
2018-11-08 16:42:22 -0200

    Update database.php

--- php/database.php
+++ php/database.php
@@ -1,6 +1,6 @@
 <?php
 
-	$con = mysqli_connect("172.30.50.101", "admin", "copelmais", "copelmais");
+	$con = mysqli_connect("172.30.50.101:3306", "admin", "copelmais", "copelmais");
 	//$con = mysqli_connect("localhost", "root", "root", "copelmais");
 
 ?>
